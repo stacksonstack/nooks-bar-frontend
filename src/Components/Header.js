4d667cc4744d13e3ab2e79d259a4fc82abdcab03
stacksonstack <staceycarrillo44@gmail.com>
2020-12-22 19:23:12 -0500

    final logo created & implemented

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,6 @@
 function Header(props) {
     return (
       <div id="bar-sign-container">
-        {/* <img alt="Nook's Bar" src={`/img/barLogo2.png`} id="bar-sign"/> */}
         <img alt="Nook's Bar" src={`/img/barLogo3.png`} id="bar-sign"/>
         
       </div>
